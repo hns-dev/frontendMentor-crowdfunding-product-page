8e9febaeb79d74736053845ddebd6a3fb0791d08
hns-dev <hekma.sh@gmial.com>
2021-02-26 08:19:21 +0300

    Fixed stats section, added close menu icon, fixed main element size.

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,12 @@
-const hamburger = document.querySelector('#hamburger-icon');
+const navBtn = document.querySelector('#nav-btn');
 const navLinks = document.querySelector('#nav-links');
+const hamburger = document.querySelector('#hamburger-icon');
+const close = document.querySelector('#close-icon');
 
-hamburger.addEventListener('click', () => {
+
+navBtn.addEventListener('click', () => {
     navLinks.classList.toggle('show');
+    hamburger.classList.toggle('show');
+    close.classList.toggle('show');
+
 })
