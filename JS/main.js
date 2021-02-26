fa08a3ad21fe5606ecd7d0d042d7839f9b76023b
hns-dev <hekma.sh@gmial.com>
2021-02-26 14:51:33 +0300

    Completed selection and success modals styles.

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,23 @@
 const navBtn = document.querySelector('#nav-btn');
 const navLinks = document.querySelector('#nav-links');
 const hamburger = document.querySelector('#hamburger-icon');
-const close = document.querySelector('#close-icon');
+const close = document.querySelector('#menu-close-icon');
+const bookmark = document.querySelector('#bookmark');
+
+
+
+const modalSelectionBtn = document.querySelectorAll('.modal-selction-btn');
+const selectionmodal = document.querySelector('.selection-modal');
+
+const modalClosebtn = document.querySelector('.modal-close-icon');
+
+
+
+const successModal = document.querySelector('.success-modal');
+const succesModalbtn = document.querySelector('#success-modal-btn');
+
+
+
 
 
 navBtn.addEventListener('click', () => {
@@ -9,4 +25,23 @@ navBtn.addEventListener('click', () => {
     hamburger.classList.toggle('show');
     close.classList.toggle('show');
 
+})
+
+bookmark.addEventListener('click', ()=>{
+    bookmark.classList.toggle('bookmarked');
+})
+
+
+modalSelectionBtn.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+        selectionmodal.style.display = 'flex';
+    })    
+})
+
+modalClosebtn.addEventListener('click', () =>{
+    selectionmodal.style.display = 'none';
+})
+
+succesModalbtn.addEventListener('click', () => {
+    successModal.style.display = 'none';
 })
